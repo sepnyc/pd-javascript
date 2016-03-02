4692667795da984badd03e7005973e2e43c3bb4a
Tanner Welsh <tannerwelsh@gmail.com>
2016-03-02 10:49:33 -0500

    Add greater board functionality

--- session-5/game/game.js
+++ session-5/game/game.js
@@ -1,31 +1,91 @@
 console.log("The file 'game.js' has been loaded.");
 
-var game = {
+var showMessage = function(message) {
+  var container = document.querySelector('#message');
+  container.innerText = message;
+}
+
+var BOARD = {
+  clickLog: [],
+
+  clearLog: function() {
+    this.clickLog = [];
+  },
+
+  greenClicked: function() {
+    this.clickLog.push('green');
+  },
+
+  redClicked: function() {
+    this.clickLog.push('red');
+  },
+
+  yellowClicked: function() {
+    this.clickLog.push('yellow');
+  },
+
+  blueClicked: function() {
+    this.clickLog.push('blue');
+  },
+
+  flashPattern: function(pattern) {
+    if (!pattern || pattern.length == 0) {
+      return false;
+    }
+
+    var current = pattern[0];
+    var self = this;
+
+    this.flash(current, function() {
+      self.flashPattern(pattern.slice(1));
+    });
+  },
+
+  flash: function(color, nextFlash) {
+    var box = document.querySelector('#' + color);
+
+    box.style['opacity'] = 0.2;
+
+    setTimeout(function() {
+      box.style['opacity'] = 1;
+
+      if (nextFlash) {
+        nextFlash();
+      }
+    }, 500);
+  }
+}
+
+var GAME = {
   pattern: [],
   echo: [],
   colors: ['green', 'red', 'yellow', 'blue'],
 
   play: function() {
+    BOARD.flashPattern(this.pattern);
+
     while (this.isCorrect()) {
       this.addOneColor();
-      alert(this.pattern);
-      this.echo.push(prompt("Do you remember the pattern?"));
+      showMessage(this.pattern);
+      BOARD.flashPattern(this.pattern);
+      showMessage("Do you remember the pattern?");
+      // this.echo.push(prompt("Do you remember the pattern?"));
 
       if (this.isCorrect()){
-        alert("You're right!");
+        showMessage("You're right!");
       } else {
-        alert("Sorry, Charlie.");
+        showMessage("Sorry, Charlie.");
       }
     }
 
-    alert("Game over.");
+    showMessage("Game over.");
   },
 
   isCorrect: function() {
     if (this.pattern.join('') === this.echo.join('')) {
       return true;
     }
-    else{
+    else {
       return false;
     }
   },
@@ -43,22 +103,3 @@ var game = {
     this.pattern.push(this.randomColor());
   }
 };
-
-var board = {
-  greenClicked: function() {
-
-  },
-
-  redClicked: function() {
-
-  },
-
-  yellowClicked: function() {
-
-  },
-
-  blueClicked: function() {
-
-  },
-
-}
